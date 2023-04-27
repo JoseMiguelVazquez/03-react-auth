d776d6169165540e4a1929e100516ade1bcd9382
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-04-26 19:45:48 -0600

    React router DOM

---
+++ src/routes/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home, Dashboard, Login, Signup, Secret } from '@/pages'
+
+const RoutesIndex = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/secret' element={<Secret />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  )
+}
+
+export default RoutesIndex
