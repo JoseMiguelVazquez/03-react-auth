325f463b7a0c1328dcb8f1cdc2d50763a5627e2d
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-05-03 21:15:16 -0600

    ✨ dashboard with user info

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useAuthContext } from '@/context/AuthContext'
+import { getSingleUser } from '@/services/userServices'
 
 const Dashboard = () => {
+  const { userPayload } = useAuthContext()
+  const [userData, setUserData] = useState({})
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await getSingleUser(userPayload.id)
+        if (response.status === 200) {
+          setUserData(response.data)
+        }
+      } catch (error) {
+        console.log('Ocurrió un error: ' + error.message)
+      }
+    }
+    fetchUserData()
+  }, [])
+
   return (
-    <h1>
-      Dashboard
-    </h1>
+    <>
+      <h1>
+        Dashboard
+      </h1>
+      {
+        userData?.first_name && <p>First Name: {userData.first_name}</p>
+      }
+      {
+        userData?.last_name && <p>Last Name: {userData.last_name}</p>
+      }
+      {
+        userData?.gender && <p>Gender: {userData.gender}</p>
+      }
+      {
+        userData?.email && <p>Email: {userData.email}</p>
+      }
+    </>
   )
 }
 
